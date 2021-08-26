bfce0e7290a25fcfbecfd06262ff1080cab992b8
Nelson Wong <nelsonwong107@gmail.com>
2021-08-26 13:50:28 -0400

    Average grade logic corrected.

--- GradeCalculator.cpp
+++ GradeCalculator.cpp
@@ -9,18 +9,47 @@ int main(void)
 	double averageGrade = 0;
 	double gradeTotal = 0;
 
-	cout << "How many courses were taken? \n";
+	cout << "How many courses were taken?" << "\n";
 	cin >> numOfCourses;
 
 	for (int i = 0; i < numOfCourses; i++)
 	{
-		cout << "Enter the grade for each class: \n" ;
+		cout << "Enter the grade for each class: \n";
 		cin >> grades;
 
 		gradeTotal += grades;
 		averageGrade = gradeTotal / numOfCourses;
 	}
-	cout << "The average grade is: \n" << averageGrade;
+	cout << "The average grade is:\n" << averageGrade;
+
+	if (averageGrade > 9 || averageGrade <= 10)
+	{
+		cout << "Grade is S.";
+	}
+	else if (averageGrade > 8 || averageGrade <= 9)
+	{
+		cout << "Grade is A.";
+	}
+	else if (averageGrade > 7 || averageGrade <= 8)
+	{
+		cout << "Grade is B.";
+	}
+	else if (averageGrade > 6 || averageGrade <= 7)
+	{
+		cout << "Grade is C.";
+	}
+	else if (averageGrade > 5 || averageGrade <= 6)
+	{
+		cout << "Grade is D.";
+	}
+	else if (averageGrade < 5 && averageGrade > 0)
+	{
+		cout << "Grade is E.";
+	}
+	else
+	{
+		cout << "Grade is F.";
+	}
 
 	return 0;
 }
